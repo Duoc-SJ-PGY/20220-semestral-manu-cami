a26009e51af25e7237eb6a5e75b3e66fe79aedc6
Magnus-Lekter <101759536+Magnus-Lekter@users.noreply.github.com>
2022-11-18 00:52:09 -0300

    Add files via upload

--- src/app/configuracion/configuracion.page.ts
+++ src/app/configuracion/configuracion.page.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
@@ -46,50 +45,4 @@ export class ConfiguracionPage implements OnInit {
   }
 
 }
-=======
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-configuracion',
-  templateUrl: './configuracion.page.html',
-  styleUrls: ['./configuracion.page.scss'],
-})
-export class ConfiguracionPage implements OnInit {
-
-  listaConfGest: Array<any> = [
-  {
-    id: 1,
-    nombre: 'Ubicación',
-    icono: 'location-outline',
-  },
-  {
-    id: 2,
-    nombre: 'Notificaciones',
-    icono: 'notifications-outline',
-  },
-  ];
-  listaConfLegal: Array<any> = [
-  {
-    id: 1,
-    nombre: 'Condiciones de servicio',
-    icono: 'document',
-  },
-  {
-    id: 2,
-    nombre: 'Política de privacidad',
-    icono: 'warning-outline',
-  },
-  {
-    id: 3,
-    nombre: 'Agradecimientos',
-    icono: 'color-wand-outline',
-  },
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
->>>>>>> 5192edfb4e6a0d0ab99b01474280bb62b835cf38
