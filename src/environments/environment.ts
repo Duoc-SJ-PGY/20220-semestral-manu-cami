808e30627157155ed3964cb0d4183f36ab6ac650
Magnus-Lekter <101759536+Magnus-Lekter@users.noreply.github.com>
2022-12-14 21:04:06 -0300

    adasdsad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const environment = {  
+  production: false,
+  google_maps_api_key: 'AIzaSyBB7oeWqeaKgW47a0595_48IcluebWA0EE',
+  firebase: {
+    apiKey: "AIzaSyBB7oeWqeaKgW47a0595_48IcluebWA0EE",
+    authDomain: "lapileria.firebaseapp.com",
+    projectId: "lapileria",
+    storageBucket: "lapileria.appspot.com",
+    messagingSenderId: "568672636952",
+    appId: "1:568672636952:web:c38e14d774545453d5f36d",
+    measurementId: "G-JEKZR66XQJ",
+  },
 };
 
 /*
